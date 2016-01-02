c9e2e3193ab77ad78ab003d1f589c65ff36aad7b
Rózsa Dániel <rozsa.daniel@ehok.u-szeged.hu>
2016-01-02 14:58:17 +0100

    ProductPage and JS

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,9 +1,23 @@
 class ReviewsController < InheritedResources::Base
 
+  def create
+  	@review = Review.new(review_params)
+  	respond_to do |format|
+    if @review.save
+      format.html { redirect_to @review, notice: 'User was successfully created.' }
+      format.js   {}
+      format.json { render json: @review, status: :created, location: @review }
+    else
+      format.html { render action: "new" }
+      format.json { render json: @review.errors, status: :unprocessable_entity }
+    end
+  end
+  end
+
   private
 
     def review_params
-      params.require(:review).permit(:title, :description, :user_id)
+      params.require(:review).permit(:description, :user_id, :product_id)
     end
 end
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+	def placeholder_image width, height, class_names = :thumbnail
+		image_tag "http://placehold.it/#{width}x#{height}", class: class_names
+	end
 end
